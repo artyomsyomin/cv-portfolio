e0bd6d6264c15a018e0bb3f6bb307c7e69a3dffe
Artyom <seminartem98@mail.ru>
2020-11-26 20:58:50 +0700

    Finished portfolio

--- src/components/main/projects/ProjectItem/ProjectItem.js
+++ src/components/main/projects/ProjectItem/ProjectItem.js
@@ -1,30 +1,23 @@
 import React from 'react';
-import {projectData} from '../../../../data/projectsData';
+import { projectData } from '../../../../data/projectsData';
 
 const ProjectItem = () => {
-
-    const projectElements = projectData.map(elem => {
-        return (
-            <div key={elem.id} className="col-md-4">
-            <div id="proj-item">
-              <span>
-                <i className={elem.icon}></i>
-              </span>
-        <h3>{elem.title}</h3>
-              <p className="used-tech">Used technologies:</p>
-              <p>{elem.technologies}</p>
-              <a href={elem.openLink}>Open Project</a>
-              <a href={elem.gitLink}>GitHub Page</a>
-            </div>
-          </div>
-        )
-    })
-
+  const projectElements = projectData.map((elem) => {
     return (
-        <>
-        {projectElements}
-        </>
-    )
-}
+      <div key={elem.id} className="col-md-4" id="proj-item">
+        <span>
+          <i className={elem.icon}></i>
+        </span>
+        <h3>{elem.title}</h3>
+        <p className="used-tech">Used technologies:</p>
+        <p>{elem.technologies}</p>
+        <a href={elem.openLink}>Open Project</a>
+        <a href={elem.gitLink}>GitHub Page</a>
+      </div>
+    );
+  });
+
+  return <>{projectElements}</>;
+};
 
 export default ProjectItem;
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,51 +1,77 @@
 export const projectData = [
-    {
-        id: 1,
-        icon: 'fas fa-hamburger fa-5x',
-        title: 'Burger Building app',
-        technologies: 'React, Redux, Hooks, html, Css...',
-        gitLink: '#todit',
-        openLink: '#toapp'
-    },
-    {
-        id: 2,
-        icon: 'fas fa-hamburger fa-5x',
-        title: 'Burger Building app',
-        technologies: 'React, Redux, Hooks, html, Css...',
-        gitLink: '#todit',
-        openLink: '#toapp'
-    },
-    {
-        id: 3,
-        icon: 'fas fa-hamburger fa-5x',
-        title: 'Burger Building app',
-        technologies: 'React, Redux, Hooks, html, Css...',
-        gitLink: '#todit',
-        openLink: '#toapp'
-    },
-    {
-        id: 4,
-        icon: 'fas fa-hamburger fa-5x',
-        title: 'Burger Building app',
-        technologies: 'React, Redux, Hooks, html, Css...',
-        gitLink: '#todit',
-        openLink: '#toapp'
-    },
-    {
-        id: 5,
-        icon: 'fas fa-hamburger fa-5x',
-        title: 'Burger Building app',
-        technologies: 'React, Redux, Hooks, html, Css...',
-        gitLink: '#todit',
-        openLink: '#toapp'
-    },
-    {
-        id: 6,
-        icon: 'fas fa-hamburger fa-5x',
-        title: 'Burger Building app',
-        technologies: 'React, Redux, Hooks, html, Css...',
-        gitLink: '#todit',
-        openLink: '#toapp'
-    },
-]
-
+  {
+    id: 1,
+    icon: 'fas fa-hamburger fa-5x',
+    title: 'Burger Building app',
+    technologies:
+      'React, Redux, Router, Hooks, HTML, CSS, Firebase(DB, Auth, Hosting), LocalStorage',
+    gitLink: 'https://github.com/artyomsyomin/burger-builder-app',
+    openLink: 'https://burger-builder-app-react-d6624.web.app/',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-portrait fa-5x',
+    title: 'CV / Portfolio',
+    technologies: 'React, Hooks, HTML, CSS, Bootstrap, Firebase',
+    gitLink: 'https://github.com/artyomsyomin/cv-portfolio',
+    openLink: '#top',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-calendar-check fa-5x',
+    title: 'To Do App (v1)',
+    technologies:
+      'React, Hooks, HTML, CSS, LocalStorage, MaterializeCSS, Firebase',
+    gitLink: 'https://github.com/artyomsyomin/todoapp-v1-react-hooks',
+    openLink: 'https://to-do-app-v1.web.app/',
+  },
+  {
+    id: 4,
+    icon: 'fas fa-guitar fa-5x',
+    title: 'Guitar Learning Bot',
+    technologies: 'JavaScript, Node.js, Telegraf, Rest Api, Heroku',
+    gitLink: 'https://github.com/artyomsyomin/guitarlearningbot-worker/',
+    openLink: 'https://t.me/guitarlearningbot',
+  },
+  {
+    id: 5,
+    icon: 'fas fa-tasks fa-5x',
+    title: 'To Do App (v2)',
+    technologies: 'React, Redux, States, HTML, CSS, LocalStorage, Firebase',
+    gitLink: 'https://github.com/artyomsyomin/todoapp-v2-react-redux',
+    openLink: 'https://to-do-app-v2.web.app/',
+  },
+  {
+    id: 6,
+    icon: 'fas fa-dog fa-5x',
+    title: 'TinDog SPA',
+    technologies: 'HTML, CSS, Bootstrap',
+    gitLink: 'https://github.com/artyomsyomin/tinDogSite',
+    openLink: 'https://artyomsyomin.github.io/tinDogSite/',
+  },
+  {
+    id: 7,
+    icon: 'fas fa-wallet fa-5x',
+    title: 'Money Converter',
+    technologies: 'React, States, Router, API, HTML, CSS, Firebase',
+    gitLink: 'https://github.com/artyomsyomin/money-rate-changer',
+    openLink: 'https://money-changer-app-9fc72.web.app/',
+  },
+  {
+    id: 8,
+    icon: 'fas fa-check-circle fa-5x',
+    title: 'To Do App (v3)',
+    technologies: 'Node.js, MongoDB, Express, Mongoose, Ejs, HTML CSS, Heroku',
+    gitLink: 'https://github.com/artyomsyomin/todo-app-v3',
+    openLink: 'https://infinite-everglades-39047.herokuapp.com/',
+  },
+  {
+    id: 9,
+    icon: 'fas fa-user-plus fa-5x',
+    title: 'Subscription Form',
+    technologies:
+      'Node.js, Express, BodyParser, API, HTML CSS, Heroku, Bootstrap',
+    gitLink: 'https://github.com/artyomsyomin/sign-up-form-mailchimp',
+    openLink: 'https://signup-form-hb.herokuapp.com/',
+  },
+];
